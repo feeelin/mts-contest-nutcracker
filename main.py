232cd581697d5a0ec2008f86b5564d0f71bd440e
Victor Feelin <valiklfiatoff@yandex.ru>
2023-01-01 23:35:51 +0300

    Second and third paragraphs
    Added second and third paragraphs

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 from typing import Dict
-
+import datetime as dt
 Nutcracker: dict[str, str] = {}
 Mari: dict[str, str] = {}
 crowd: dict[str, str] = {}
+castle: dict[str, str] = {}
+
 
 ########################################################################################################################
 
@@ -37,3 +39,29 @@ def ProofedByBugormister():
 def remember(conditer):
     crowd.clear()
     crowd['thinkAbout'] = ['life', 'himself']
+
+thisMinute = dt.datetime.now()
+
+def scream(Mari):
+    Mari['condition'] = 'delight'
+    def noticed(Mari):
+        castle['illuminated by'] = 'pink light'
+        castle['turrets'] = {
+            'condition': ['lightweight', 'air'],
+            'wallsCoveredBy': {
+                'bouquets of beautiful flowers': ['violets', 'daffodils', 'tulips', 'gillyflower'],
+                'shimmeredDelightfully' : True
+            }
+        }
+        castle['middleDome'] = {
+            'big' : True,
+            'stars' : {
+            'color': 'gold',
+            'sparkled': True,
+        },
+        castle['pyramidalRoofs']: castle['middleDome']
+        }
+
+def nutcrackerResult():
+    Mari['location'] = 'Marzipan Castle'
+    Nutcracker['location'] = Mari['location']
